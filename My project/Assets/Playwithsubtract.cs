25f5a5991306efcb185982f902c1ecc529a9e81b
sarahchowdhury <37392789+sarahchowdhury@users.noreply.github.com>
2023-10-05 16:12:40 -0700

    Changes by Sultana

--- My project/Assets/Playwithsubtract.cs
+++ My project/Assets/Playwithsubtract.cs
@@ -11,7 +11,8 @@ public class Playwithsubtract : MonoBehaviour
     public Transform Dog;
     public Transform Monster;
     public float speed;
-    //public TextMeshPro Question;
+    //public Text Question;
+    public TextMeshProUGUI Question,Answer1,Answer2,Answer3;
 
     // Start is called before the first frame update
     void Start()
@@ -26,6 +27,45 @@ public class Playwithsubtract : MonoBehaviour
         {
             Debug.Log("No object");
         }*/
+        QuestionGenerate();
+    }
+    public void QuestionGenerate()
+    {
+        int value1 = Random.Range(1, 10);
+        int value2 = Random.Range(1, 10);
+        int answer;
+        if (value1 > value2)
+        {
+            Question.text = " " + value1 + " - " + value2 + " = ?";
+            answer = value1-value2;
+        }
+        else {
+            Question.text = " " + value2 + " - " + value1 + " = ?";
+            answer = value2-value1;
+        }
+        int i= Random.Range(1, 3);
+        if (i == 1)
+        {
+            Answer1.text = answer + "";
+            Answer2.text = Random.Range(1, 10)+"";
+            Answer3.text = Random.Range(1, 10)+"";
+        }
+        else if(i == 2)
+        {
+            Answer2.text = answer + "";
+            Answer1.text = Random.Range(1, 10)+"";
+            Answer3.text = Random.Range(1, 10)+"";
+        }
+        else
+        {
+            Answer3.text = answer + "";
+            Answer2.text = Random.Range(1, 10)+"";
+            Answer1.text = Random.Range(1, 10)+"";
+        }
+       
+        
+        
+        Debug.Log(Question.text);
     }
     public void FixedUpdate()
     {
