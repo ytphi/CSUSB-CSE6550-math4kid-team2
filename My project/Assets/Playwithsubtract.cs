b423cfe877a4531ebd4e8f66b867ea1c434e8626
sarahchowdhury <37392789+sarahchowdhury@users.noreply.github.com>
2023-10-06 11:26:07 -0700

    changes by sultana

--- My project/Assets/Playwithsubtract.cs
+++ My project/Assets/Playwithsubtract.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 
 
@@ -12,60 +13,55 @@ public class Playwithsubtract : MonoBehaviour
     public Transform Monster;
     public float speed;
     //public Text Question;
-    public TextMeshProUGUI Question,Answer1,Answer2,Answer3;
+    public TextMeshProUGUI Question, Answer1, Answer2, Answer3;
+    public int value1, value2, answer;
+    public static int consequtive_ques_no = 5;
 
     // Start is called before the first frame update
     void Start()
     {
 
-        //Question= GameObject.Find("Question_panel/Question_text").GetComponent<Text>();
-        /*if(Question != null)
-        {
-            Question.text = Random.Range(1, 10)+"";
-        }
-        else
-        {
-            Debug.Log("No object");
-        }*/
+
         QuestionGenerate();
+        //CheckAnswer(button_answer,sceneName1,sceneName2);
     }
     public void QuestionGenerate()
     {
-        int value1 = Random.Range(1, 10);
-        int value2 = Random.Range(1, 10);
-        int answer;
+        value1 = Random.Range(1, 10);
+        value2 = Random.Range(1, 10);
+
         if (value1 > value2)
         {
             Question.text = " " + value1 + " - " + value2 + " = ?";
-            answer = value1-value2;
+            answer = value1 - value2;
         }
         else {
             Question.text = " " + value2 + " - " + value1 + " = ?";
-            answer = value2-value1;
+            answer = value2 - value1;
         }
-        int i= Random.Range(1, 3);
+        int i = Random.Range(1, 3);
         if (i == 1)
         {
             Answer1.text = answer + "";
-            Answer2.text = Random.Range(1, 10)+"";
-            Answer3.text = Random.Range(1, 10)+"";
+            Answer2.text = Random.Range(1, 10) + "";
+            Answer3.text = Random.Range(1, 10) + "";
         }
-        else if(i == 2)
+        else if (i == 2)
         {
             Answer2.text = answer + "";
-            Answer1.text = Random.Range(1, 10)+"";
-            Answer3.text = Random.Range(1, 10)+"";
+            Answer1.text = Random.Range(1, 10) + "";
+            Answer3.text = Random.Range(1, 10) + "";
         }
         else
         {
             Answer3.text = answer + "";
-            Answer2.text = Random.Range(1, 10)+"";
-            Answer1.text = Random.Range(1, 10)+"";
+            Answer2.text = Random.Range(1, 10) + "";
+            Answer1.text = Random.Range(1, 10) + "";
         }
+        
+
+
        
-        
-        
-        Debug.Log(Question.text);
     }
     public void FixedUpdate()
     {
@@ -75,6 +71,30 @@ public class Playwithsubtract : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+    }
+    public void CheckAnswer(TextMeshProUGUI button_answer)//, string sceneName1, string sceneName2)
+    {
+        if (button_answer.text == answer.ToString())
+        {
+            if(consequtive_ques_no!=0)
+            {
+                consequtive_ques_no--;
+                QuestionGenerate();
+            }
+            //SceneManager.LoadScene("mainmenuscene");
+            Debug.Log("correct");
+        }
+        else
+        {
+            if (consequtive_ques_no != 0)
+            {
+                consequtive_ques_no--;
+                QuestionGenerate();
+            }
+            //SceneManager.LoadScene("scene4_tryagain");
+            Debug.Log("wrong");
+        }
         
     }
 }
