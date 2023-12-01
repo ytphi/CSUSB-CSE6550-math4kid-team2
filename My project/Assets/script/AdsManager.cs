2b63e2cb39e710d8939d063d358a45535f2cf5aa
sarahchowdhury <37392789+sarahchowdhury@users.noreply.github.com>
2023-11-30 16:17:52 -0800

    changes by sultana

--- My project/Assets/script/AdsManager.cs
+++ My project/Assets/script/AdsManager.cs
@@ -34,7 +34,7 @@ public class AdsManager : MonoBehaviour
         }
 
         // Create a 320x50 banner at top of the screen
-        _bannerView = new BannerView(_adUnitId, AdSize.Banner, AdPosition.TopRight);
+        _bannerView = new BannerView(_adUnitId, AdSize.Banner, AdPosition.Top);
         ListenToAdEvents();
     }
 
--- My project/Assets/script/ApplicationExit.cs
+++ My project/Assets/script/ApplicationExit.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Microsoft.AppCenter.Unity.Analytics;
 
 public class ApplicationExit : MonoBehaviour
 {
     // Start is called before the first frame update
-   public void OnExitButtonClick()
+    public void OnExitButtonClick()
     {
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
+        Debug.Log("Exit");
 #else
-        Application.Quit();
+         Application.Quit();
+         Debug.Log("Exit from android");
 #endif
     }
 }
